949f07a04318bfab7e0893b242544d15c27be668
unknown <poncemark98@gmail.com>
2026-04-22 19:52:24 +0800

    update app.py

--- app/app.py
+++ app/app.py
@@ -28,13 +28,48 @@ df = load_data()
 st.title("📊 DashIQ — Sales Dashboard")
 
 # -----------------------------
+# SIDEBAR FILTERS
+# -----------------------------
+st.sidebar.header("🔍 Filters")
+# Year filter
+years = sorted(df["Year"].dropna().unique())
+selected_year = st.sidebar.multiselect(
+    "Select Year",
+    options=years,
+    default=years
+)
+regions = sorted(df["Region"].dropna().unique())
+selected_region = st.sidebar.multiselect(
+    "Select Region",
+    options=regions,
+    default=regions
+)
+categories = sorted(df["Item Type"].dropna().unique())
+selected_category = st.sidebar.multiselect(
+    "Select Category",
+    options=categories,
+    default=categories
+)   
+products = sorted(df["Item Type"].dropna().unique())
+selected_product = st.sidebar.multiselect(
+    "Select Product",
+    options=products,
+    default=products
+)
+filtered_df = df[
+    (df["Year"].isin(selected_year)) &
+    (df["Region"].isin(selected_region)) &
+    (df["Item Type"].isin(selected_category)) &
+    (df["Item Type"].isin(selected_product))
+]
+# -----------------------------
 # KPI SECTION
 # -----------------------------
 col1, col2, col3, col4 = st.columns(4)
 
-total_revenue = df["Total Revenue"].sum()
-total_profit = df["Total Profit"].sum()
-total_orders = df["Order ID"].nunique()
+total_revenue = filtered_df["Total Revenue"].sum()
+total_profit = filtered_df["Total Profit"].sum()
+total_orders = filtered_df["Order ID"].nunique()
 profit_margin = total_profit / total_revenue
 
 col1.metric("Revenue", f"${total_revenue:,.0f}")
